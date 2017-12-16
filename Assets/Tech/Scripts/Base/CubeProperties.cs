9256c1dc24ca55f82f57d6979717b59920270d68
Dogelix <yeatmanjake@gmail.com>
2017-12-16 17:58:11 +0000

    Ship Building Game Intial Commit
    Just the first of many commits. This is all the work done thus far.

---
+++ Assets/Tech/Scripts/Base/CubeProperties.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeProperties : MonoBehaviour
+{
+    [SerializeField]
+    bool invinceable = false;
+    [SerializeField]
+    bool engine = false;
+    [SerializeField]
+    bool fuelTank = false;
+    [SerializeField]
+    bool generator = false;
+    [SerializeField]
+    bool battery = false;
+
+    private void Start()
+    {
+        if (engine)
+        {
+
+        }
+        if (fuelTank)
+        {
+
+        }
+        if (generator)
+        {
+
+        }
+        if (battery)
+        {
+
+        }
+    }
+    
+    public string GetBlockType()
+    {
+        if (engine)
+        {
+            return "engine";
+        }
+        if (fuelTank)
+        {
+            return "fuelTank";
+        }
+        if (generator)
+        {
+            return "generator";
+        }
+        if (battery)
+        {
+            return "battery";
+        }
+        else
+        {
+            return "NULL";
+        }
+    }
+
+    /// <summary>
+    /// This takes in the ray cast to see where the click is coming from, then calculate on which face
+    /// to add 1 unit to that direction to give a location to instantiate a new block.
+    /// </summary>
+    /// <param name="_rayHit">The raycast hit used to find the block to begin with</param>
+    /// <returns>This returns a Vector3 of the location to instantiate a new ship block</returns>
+    public Vector3 GetLocationToBuildBlock(RaycastHit _rayHit)
+    {
+        Vector3 _incomingVec = _rayHit.normal - Vector3.up;
+
+        if (_incomingVec == new Vector3(0, -1, -1))
+            return Vector3.back;
+        if (_incomingVec == new Vector3(0, -1, 1))
+            return Vector3.forward;
+        if (_incomingVec == new Vector3(0, 0, 0))
+            return Vector3.up;
+        if (_incomingVec == new Vector3(1, 1, 1))
+            return Vector3.down;
+        if (_incomingVec == new Vector3(-1, -1, 0))
+            return Vector3.left;
+        if (_incomingVec == new Vector3(1, -1, 0))
+            return Vector3.right;
+
+        return new Vector3(0, 0, 0);
+    }
+
+
+
+    /// <summary>
+    /// To be called on right click to destroy a block if !invincible
+    /// </summary>
+    void DestroyBlock()
+    {
+        if (!invinceable)
+        {
+            //Destroy(this);
+        }
+    }
+}
---
+++ Assets/Tech/Scripts/Base/MouseManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour {
+
+    [SerializeField]
+    GameObject cubeBuild;
+
+    Ray ray;
+    RaycastHit hit;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            BuildBlock();
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("RB Down");
+        }
+    }
+
+
+    Collider GetHitCollider()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            return hit.collider;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    bool CheckForCube()
+    {
+        if(GetHitCollider().tag == "buildableCube")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+    void BuildBlock()
+    {
+        if (CheckForCube())
+        {
+            Collider _collider = GetHitCollider();
+            CubeProperties _cubePropertiesScript = _collider.GetComponent<CubeProperties>();
+            Vector3 locationToBuild = _cubePropertiesScript.GetLocationToBuildBlock(hit);
+            Instantiate(cubeBuild, locationToBuild, new Quaternion(0, 0, 0, 0));
+            Debug.Log("Check For Cube Done");   
+        }
+    }
+}
